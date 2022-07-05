3a97074f216fb9b14062f0c5e7155ce53014fa77
Pedro <92195721+PedroHenriqueMR@users.noreply.github.com>
2022-07-05 19:28:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {soma, subitracao, multiplicacao, divisao} from './calculo.js'
 
-let valor1 = 17;
-let valor2 = 21;
+let valor1 = 0;
+let valor2 = 10;
 
 const ad = soma(valor1,valor2)
 const su = subitracao(valor1,valor2)
@@ -9,14 +9,14 @@ const mul = multiplicacao(valor1,valor2)
 const di = divisao(valor1,valor2)
 
 
-console.log("Resultado da Adção: "+valor1+" * "+valor2+" = "+ad)
-console.log("Resultado da Subtração: "+valor1+" * "+valor2+" = "+su)
-if(mul == 0){
+console.log("Resultado da Adção: "+valor1+" + "+valor2+" = "+ad)
+console.log("Resultado da Subtração: "+valor1+" - "+valor2+" = "+su)
+if(valor1 == 0 || valor2 == 0){
     console.log("Multiplicar por zero sempre o resultado será zero!")
 }else{
     console.log("Resultado da Multiplicação: "+valor1+" * "+valor2+" = "+mul)
 };
-if(di == Infinity || di == 0){
+if(valor1 == 0 || valor2 == 0){
     console.log("Divisão por zero sempre irar retornar zero!")
 }else{
     console.log("Resultado da Divisão: "+valor1+" / "+valor2+" = "+di.toFixed(2))
